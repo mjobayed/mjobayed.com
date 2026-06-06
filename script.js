27cf363a2c096632e0b5c1f9579085e1ee147996
Md. Jobayed Islam <contact@mjobayed.com>
2026-06-06 21:31:08 +0600

    feat(js): add functionality for dark/light mode toggle button
    I need to refactor the way this is being done here.

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const prefersDark = window.matchMedia("(prefers-color-scheme: dark)");
+const themeButton = document.getElementById("themeButton");
 
 const applyColorScheme = () => {
   let isDark = prefersDark.matches;
@@ -6,7 +7,19 @@ const applyColorScheme = () => {
     "data-theme",
     isDark ? "tokyonight-night" : "tokyonight-day",
   );
+  themeButton.innerText = isDark ? "Light Theme" : "Dark Theme";
 };
 applyColorScheme();
 
 prefersDark.addEventListener("change", applyColorScheme);
+
+// TODO: refactor this to merge with applyColorScheme() somehow
+themeButton.addEventListener("click", () => {
+  let isDark =
+    document.documentElement.getAttribute("data-theme") === "tokyonight-night";
+  document.documentElement.setAttribute(
+    "data-theme",
+    !isDark ? "tokyonight-night" : "tokyonight-day",
+  );
+  themeButton.innerText = !isDark ? "Light Theme" : "Dark Theme";
+});
